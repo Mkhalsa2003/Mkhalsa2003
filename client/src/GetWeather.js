3ad823c99cee0e0f5cb4e135ddaa1c827d8502bc
nbprogramming <is5422tv@go.minnstate.edu>
2022-04-11 19:29:37 -0500

    Added css and support for kelvin

--- client/src/GetWeather.js
+++ client/src/GetWeather.js
@@ -2,7 +2,7 @@ import React, { useState, ReactDOM } from "react";
 import './App.css';
 import Axios from 'axios';
 import { Cookies } from 'react-cookie';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap';
 
 
 function GetWeather() {
@@ -18,25 +18,67 @@ function GetWeather() {
             }).then((apiResponse) => {
                 Axios.get(apiResponse.data.properties.forecast, {
                 }).then((forecastResponse) => {
-                    var header = "<tr>"
+                    var header = "<tr>";
                     var body = "<tr>";
-                    var img = "<tr>"
+                    var img = "<tr>";
+                    var header2 = "<tr>";
+                    var body2 = "<tr>";
+                    var img2 = "<tr>";
+                    if(!forecastResponse.data.properties.periods[0].isDaytime)
+                    {
+                        header += "<th class='tableheader'></th>";
+                        body += "<td class='tablebody'></td>";
+                        img += "<td class='tableimg'></td>"
+                    }
                     forecastResponse.data.properties.periods.forEach(period => {
-                        header += ("<th>" + period.name + "</th>");
-                        var tempstring = period.temperature
-                        if (forcastType == "C")
+                        if(period.isDaytime)
                         {
-                            var temp = parseInt(tempstring);
-                            var tempCalc = Math.round(((temp - 32) * .5556));
-                            var tempstring = tempCalc.toString();
+                            header += ("<th class='tableheader'>" + period.name + "</th>");
+                            var tempstring = period.temperature
+                            if (forcastType == "C")
+                            {
+                                var temp = parseInt(tempstring);
+                                var tempCalc = Math.round(((temp - 32) * .5556));
+                                var tempstring = tempCalc.toString();
+                            }
+                            else if (forcastType == "K")
+                            {
+                                var temp = parseInt(tempstring);
+                                var tempCalc = Math.round(((temp - 32) * .5556) + 273.15);
+                                var tempstring = tempCalc.toString();
+                            }
+                            body += ("<td class='tablebody'>" + tempstring + " " + forcastType + "</td>");
+                            img += ("<td class='tableimg'><img src=\"" + period.icon + "\"/></td>");
                         }
-                        body += ("<td>" + tempstring + " " + forcastType + "</td>");
-                        img += ("<td><img src=\"" + period.icon + "\"/></td>");
+                        else
+                        {
+                            header2 += ("<th>" + period.name + "</th>");
+                            var tempstring = period.temperature
+                            if (forcastType == "C")
+                            {
+                                var temp = parseInt(tempstring);
+                                var tempCalc = Math.round(((temp - 32) * .5556));
+                                var tempstring = tempCalc.toString();
+                            }
+                            else if (forcastType == "K")
+                            {
+                                var temp = parseInt(tempstring);
+                                var tempCalc = Math.round(((temp - 32) * .5556) + 273.15);
+                                var tempstring = tempCalc.toString();
+                            }
+                            body2 += ("<td>" + tempstring + " " + forcastType + "</td>");
+                            img2 += ("<td><img src=\"" + period.icon + "\"/></td>");
+
+                        }
                     });
                     header += "</tr>";
                     body += "</tr>";
                     img += "</tr>";
-                    var html = "<table>" + header + body + img + "</table>";
+                    header2 += "</tr>";
+                    body2 += "</tr>";
+                    img2 += "</tr>";
+
+                    var html = "<table>" + header + body + img + "<tr class='spacer'></tr>" + header2 + body2 + img2 + "</table>";
                     setTable(html);
                 });
             });
@@ -75,10 +117,15 @@ function GetWeather() {
             <div dangerouslySetInnerHTML={{ __html: htmlTable }} />
             <Container>
                 <Row>
-                    <Col><Button onClick={() => setUserPreference("F")}>F</Button></Col>
-                </Row>
-                <Row>
-                    <Col><Button onClick={() => setUserPreference("C")}>C</Button></Col>
+                    <Col />
+                    <Col>
+                        <Stack direction="horizontal" className="col-md-5 mx-auto">
+                            <Button onClick={() => setUserPreference("F")}>F</Button>
+                            <Button onClick={() => setUserPreference("C")}>C</Button>
+                            <Button onClick={() => setUserPreference("K")}>K</Button>
+                        </Stack>
+                    </Col>
+                    <Col />
                 </Row>
             </Container>
         </div>
